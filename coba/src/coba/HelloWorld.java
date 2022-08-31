bf5de49bb468d56518d721c224326b43d4cc431d
sulthonsidayu <sulthonuladhim@yahoo.com>
2022-08-31 20:01:10 +0700

    menambahkan kata lagi di Hello World

--- coba/src/coba/HelloWorld.java
+++ coba/src/coba/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World Lagi");
+		System.out.println("Hello World Lagi dan lagi");
 	}
 
 }
